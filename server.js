b460ef9a705b0dafcced12db0387333c2b907f62
Jozef <jozefw@sbcglobal.net>
2015-05-01 15:38:07 -0700

    make the first connectio to a server with modules

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-var http = requiree('http'),
+var http = require('http');
 http.createServer(function(request,response){
 	response.writeHead(200,{
-		"Content-Type": 'text/plain',
-
-	});
+		"Content-Type": 'text/plain'});
 	response.write('here is your server');
 	response.end();
 
 }).listen(8080);
+
+var http = require('http');
+
+function start() {
+	function onRequest() {
+		console.log('request recieved');
+		response.writeHead(200,{
+			'Content-Type': 'text/plain',
+
+		});
+		response.write("hello there");
+		response.end();
+	}
+	http.createServer(onRequest).listen(8080);
+	console.log("server started");
+}
+
+exports.start = start;
